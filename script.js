795d0ab3d219b5662957a987b9f85b735180664c
Saidbek-2011 <raxmonqulovsaidbekk@gmail.com>
2025-06-13 23:29:38 +0500

    jang

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+import { neon } from '@netlify/neon';
+const sql = neon(); // automatically uses env NETLIFY_DATABASE_URL
+const [post] = await sql`SELECT * FROM posts WHERE id = ${postId}`;
 'use strict';
+
 
 
 
